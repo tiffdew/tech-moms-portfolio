1e60a6183672974057b0f37ef56dc5e37ecf531a
Tiffany DeWitt <tiffanydewitt@Josh-DeWitts-MacBook-Pro.local>
2022-03-16 21:42:32 -0600

    JavaScript Nav-link

---
+++ components/contact/contact.js
@@ -0,0 +1,12 @@
+function setActiveLink() {
+    let links = document.getElementsByClassName('nav-link');
+
+    for (let i = 0; i < links.length; i++) {
+        console.log('CURRENT LINK:', links[i]);
+        if (links[i].innerHTML === 'CONTACT') {
+            links[i].style = 'text-decoration: underline';
+        }
+    }
+}
+
+setActiveLink();
---
+++ components/experience/experience.js
@@ -0,0 +1,29 @@
+function setActiveLink() {
+    let links = document.getElementsByClassName('nav-link');
+
+    for (let i = 0; i < links.length; i++) {
+        console.log('CURRENT LINK:', links[i]);
+        console.log(links[i].getAttribute("href"));
+        console.log(document.location.pathname);
+        if (links[i].getAttribute("href") === document.location.pathname) {
+            links[i].style = 'text-decoration: underline';
+        }
+    }
+}
+
+
+
+
+function addNav(text, url, id, cls) {
+    var nav = '<a id="' + id + '" class="' + cls + '" href="' + url + '">' + text + '</a>'
+    document.getElementById("link-test").innerHTML += nav;
+    //document.getElementById("link-test").appendChild(nav);
+
+}
+
+addNav('HOME', '/Users/tiffanydewitt/Desktop/Tech-Moms/Portfolio/index.html', 'navHome', 'nav-link')
+addNav('ABOUT', '/Users/tiffanydewitt/Desktop/Tech-Moms/Portfolio/components/about/about.html', 'navAbout', 'nav-link')
+addNav('EXPERIENCE', '/Users/tiffanydewitt/Desktop/Tech-Moms/Portfolio/components/experience/experience.html', 'navExperience', 'nav-link')
+addNav('CONTACT', '/Users/tiffanydewitt/Desktop/Tech-Moms/Portfolio/components/contact/contact.html', 'navContact', 'nav-link')
+
+setActiveLink();
