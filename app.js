38ebd8d78dd8bb07426e74aca0c43eb9f6e21aa8
Tiffany DeWitt <tiffanydewitt@Josh-DeWitts-MacBook-Pro.local>
2022-03-19 15:40:29 -0600

    fixing js link paths

--- app.js
+++ app.js
@@ -21,9 +21,9 @@ function addNav(text, url, id, cls) {
     document.getElementsByClassName("link-wrapper")[0].innerHTML += nav;
 }
 
-addNav('HOME', '/Users/tiffanydewitt/Desktop/Tech-Moms/Portfolio/index.html', 'navHome', 'nav-link')
+addNav('HOME', 'index.html', 'navHome', 'nav-link')
 addNav('ABOUT', './components/about/about.html', 'navAbout', 'nav-link')
-addNav('EXPERIENCE', '/Users/tiffanydewitt/Desktop/Tech-Moms/Portfolio/components/experience/experience.html', 'navExperience', 'nav-link')
-addNav('CONTACT', '/Users/tiffanydewitt/Desktop/Tech-Moms/Portfolio/components/contact/contact.html', 'navContact', 'nav-link')
+addNav('EXPERIENCE', './components/experience/experience.html', 'navExperience', 'nav-link')
+addNav('CONTACT', './components/contact/contact.html', 'navContact', 'nav-link')
 
 setActiveLink();
