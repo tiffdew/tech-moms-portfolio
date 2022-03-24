2d99ac41b1bf9cc209dd7b7a106d94ba934df64a
Tiffany DeWitt <tiffanydewitt@Josh-DeWitts-MacBook-Pro.local>
2022-03-23 21:37:46 -0600

    refining about page

--- app.js
+++ app.js
@@ -1,15 +1,3 @@
-//function setActiveLink() {
-//let links = document.getElementsByClassName('nav-link');
-
-//for (let i = 0; i < links.length; i++) {
-//console.log('CURRENT LINK:', links[i]);
-//console.log(links[i].getAttribute("href"));
-//console.log(document.location.pathname);
-//if (links[i].getAttribute("href") === document.location.pathname) {
-//links[i].style = 'text-decoration: underline';
-//}
-//}
-//}
 function setActiveLink() {
     let links = document.getElementsByClassName('nav-link');
 
@@ -25,15 +13,45 @@ setActiveLink();
 
 
 
-function addNav(text, url, id, cls) {
-    var nav = '<a id="' + id + '" class="' + cls + '" href="' + url + '">' + text + '</a>'
 
-    document.getElementsByClassName("link-wrapper")[0].innerHTML += nav;
-}
 
-addNav('HOME', '/tech-moms-portfolio/index.html', 'navHome', 'nav-link')
-addNav('ABOUT', '/tech-moms-portfolio/components/about/about.html', 'navAbout', 'nav-link')
-addNav('EXPERIENCE', '/tech-moms-portfolio/components/experience/experience.html', 'navExperience', 'nav-link')
-addNav('CONTACT', '/tech-moms-portfolio/components/contact/contact.html', 'navContact', 'nav-link')
+
+//function setActiveLink() {
+//let links = document.getElementsByClassName('nav-link');
+
+//for (let i = 0; i < links.length; i++) {
+//console.log('CURRENT LINK:', links[i]);
+//console.log(links[i].getAttribute("href"));
+//console.log(document.location.pathname);
+//if (links[i].getAttribute("href") === document.location.pathname) {
+//links[i].style = 'text-decoration: underline';
+//}
+//}
+//}
+//function setActiveLink() {
+//let links = document.getElementsByClassName('nav-link');
+
+//for (let i = 0; i < links.length; i++) {
+//console.log('CURRENT LINK:', links[i]);
+//if (links[i].innerHTML === 'HOME') {
+//links[i].style = 'text-decoration: underline';
+//}
+//}
+//}
 
 setActiveLink();
+
+
+
+//function addNav(text, url, id, cls) {
+    //var nav = '<a id="' + id + '" class="' + cls + '" href="' + url + '">' + text + '</a>'
+
+    //document.getElementsByClassName("link-wrapper")[0].innerHTML += nav;
+//}
+
+//addNav('HOME', 'index.html', 'navHome', 'nav-link')
+//addNav('ABOUT', 'components/about/about.html', 'navAbout', 'nav-link')
+//addNav('EXPERIENCE', '/tech-moms-portfolio/components/experience/experience.html', 'navExperience', 'nav-link')
+//addNav('CONTACT', '/tech-moms-portfolio/components/contact/contact.html', 'navContact', 'nav-link')
+
+//setActiveLink();
