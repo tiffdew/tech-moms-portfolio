47eeb7e8fe6f667421acdc99490d2c07ba2e9d5c
Tiffany DeWitt <tiffanydewitt@Josh-DeWitts-MacBook-Pro.local>
2022-03-24 19:35:32 -0600

    fixing underlining links

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ setActiveLink();
 //}
 //}
 
-setActiveLink();
+//setActiveLink();
 
 
 
--- components/experience/experience.js
+++ components/experience/experience.js
@@ -9,19 +9,6 @@ function setActiveLink() {
     }
 }
 
+setActiveLink()
 
 
-
-function addNav(text, url, id, cls) {
-    var nav = '<a id="' + id + '" class="' + cls + '" href="' + url + '">' + text + '</a>'
-    document.getElementById("link-test").innerHTML += nav;
-    //document.getElementById("link-test").appendChild(nav);
-
-}
-
-addNav('HOME', '/Users/tiffanydewitt/Desktop/Tech-Moms/Portfolio/index.html', 'navHome', 'nav-link')
-addNav('ABOUT', '/Users/tiffanydewitt/Desktop/Tech-Moms/Portfolio/components/about/about.html', 'navAbout', 'nav-link')
-addNav('EXPERIENCE', '/Users/tiffanydewitt/Desktop/Tech-Moms/Portfolio/components/experience/experience.html', 'navExperience', 'nav-link')
-addNav('CONTACT', '/Users/tiffanydewitt/Desktop/Tech-Moms/Portfolio/components/contact/contact.html', 'navContact', 'nav-link')
-
-setActiveLink();
